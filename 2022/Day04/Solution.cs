e766085f32b19ba6068107cabea927475dff655b
Eric De Carufel <eric.decarufel@gsoft.com>
2022-12-04 16:21:03 -0500

    2022-04 refactoring

--- 2022/Day04/Solution.cs
+++ 2022/Day04/Solution.cs
@@ -12,17 +12,15 @@ class Solution : Solver {
 
     public object PartOne(string input) =>
         (from line in input.SplitLine()
-        let parts = line.Split(",").SelectMany(p => p.Split("-")).Select(n => Int32.Parse(n)).ToArray()
+        let parts = line.Split(",").SelectMany(p => p.Split("-")).Select(int.Parse).ToArray()
         where parts[0] >= parts[2] && parts[1] <= parts[3] ||
             parts[2] >= parts[0] && parts[3] <= parts[1]
         select line).Count();        
 
     public object PartTwo(string input) =>
-            (from line in input.SplitLine()
-        let parts = line.Split(",").SelectMany(p => p.Split("-")).Select(n => Int32.Parse(n)).ToArray()
-        let range1 = Enumerable.Range(parts[0], parts[1] - parts[0] + 1)
-        let range2 = Enumerable.Range(parts[2], parts[3] - parts[2] + 1)
-        where range1.Intersect(range2).Count() > 0
+        (from line in input.SplitLine()
+        let parts = line.Split(",").SelectMany(p => p.Split("-")).Select(int.Parse).ToArray()
+        where parts[1] >= parts[2] && parts[0] <= parts[3] ||
+            parts[2] >= parts[1] && parts[3] <= parts[0]
         select line).Count();        
-
 }
